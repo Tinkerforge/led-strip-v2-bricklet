d668e02d6d581506180440dea7caa39062fb41af
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:12:06 +0100

    Add go examples.

---
+++ software/examples/go/example_callback.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+    "tinkerforge/led_strip_v2_bricklet"
+)
+
+// FIXME: This example is incomplete
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your LED Strip Bricklet 2.0.
+
+func main() {
+	ipcon := ipconnection.New()
+    defer ipcon.Close()
+	ls, _ := led_strip_v2_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+    defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Set frame duration to 50ms (20 frames per second)
+	ls.SetFrameDuration(50)
+
+	ls.RegisterFrameStartedCallback(func(length uint16) {
+		fmt.Println("Length: ", length)
+	})
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+	ipcon.Disconnect()
+}
---
+++ software/examples/go/example_simple.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/led_strip_v2_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your LED Strip Bricklet 2.0.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	ls, _ := led_strip_v2_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Set first 3 LEDs to red, green and blue
+	ls.SetLEDValues(0, []uint8{255, 0, 0, 0, 255, 0, 0, 0, 255})
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
