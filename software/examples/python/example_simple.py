53ae8d386f1ce8841d4be04d65c8de8c8db60771
Erik Fleckstein <erik@tinkerforge.com>
2020-01-13 17:20:11 +0100

    examples: Use python3 input instead of python2 raw_input.

--- software/examples/python/example_simple.py
+++ software/examples/python/example_simple.py
@@ -18,5 +18,5 @@ if __name__ == "__main__":
     # Set first 3 LEDs to red, green and blue
     ls.set_led_values(0, [255, 0, 0, 0, 255, 0, 0, 0, 255])
 
-    raw_input("Press key to exit\n") # Use input() in Python 3
+    input("Press key to exit\n") # Use raw_input() in Python 2
     ipcon.disconnect()
