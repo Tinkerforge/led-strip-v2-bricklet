667f2358361a56202a64b159416370ae369cf690
Erik Fleckstein <erik@tinkerforge.com>
2020-08-25 15:53:28 +0200

    uc: Fix whitespace

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -1,14 +1,9 @@
-
-
 #include "bindings/hal_common.h"
 #include "bindings/bricklet_led_strip_v2.h"
 
 #define UID "XYZ" // Change XYZ to the UID of your LED Strip Bricklet 2.0
 
 void check(int rc, const char* msg);
-
-
-
 
 TF_LEDStripV2 ls;
 
