c12badfb0258050c70c0ee79888349790ec10650
Matthias Bolte <matthias@tinkerforge.com>
2020-08-25 18:18:17 +0200

    uc: Adapt examples to new skeleton

--- software/examples/uc/example_callback.c
+++ software/examples/uc/example_callback.c
@@ -13,18 +13,16 @@ uint16_t idx = 0;
 bool frame_started = false;
 // Use frame started callback to trigger drawing the next frame.
 void frame_started_handler(TF_LEDStripV2 *device, uint16_t length, void *user_data) {
-	(void)device; (void) length; (void)user_data; // avoid unused parameter warning
+	(void)device; (void)length; (void)user_data; // avoid unused parameter warning
 
 	frame_started = true;
 }
-
 
 TF_LEDStripV2 ls;
 
 void example_setup(TF_HalContext *hal) {
 	// Create device object
 	check(tf_led_strip_v2_create(&ls, UID, hal), "create device object");
-
 
 	// Set frame duration to 50ms (20 frames per second)
 	check(tf_led_strip_v2_set_frame_duration(&ls, 50), "call set_frame_duration");
@@ -53,7 +51,7 @@ void example_loop(TF_HalContext *hal) {
 	if (idx >= NUM_LEDS * 3) {
 		idx = 0;
 	}
-    rgb[idx] = 255;
+	rgb[idx] = 255;
 
 	tf_led_strip_v2_set_led_values(&ls, 0, rgb, NUM_LEDS * 3);
 }
